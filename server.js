404ac769d53252790ff9981ef9052dfaf85e628e
Vinicius <masukawavini@hotmail.com>
2023-03-12 09:39:55 -0300

    Configurando o servidor

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello');
+});
+
+app.listen(3000, () => {
+	console.log('Servidor rodando');
+});
